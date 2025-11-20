e04a81d14c351fdd96bca2b1838c85a3b3798f66
Aayansh889 <aayansh.saxena@gmail.com>
2025-11-20 16:14:17 +0530

    Add files via upload

---
+++ Activity3.py
@@ -0,0 +1,12 @@
+print("Enter marks obtained in 4 subjects")
+
+math=int(input("math:"))
+science=int(input("science:"))
+sst=int(input("sst:"))
+sanskrit=int(input("sanskrit:"))
+
+sum=math+science+sst+sanskrit
+print("sum of math,science,sst and sanskrit is:",sum)
+
+perc=sum/320*100
+print("Percentage:",perc)
