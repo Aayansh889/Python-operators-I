53f8f383c3eb7f07ca7259f8e81edb491bad3ec3
Aayansh889 <aayansh.saxena@gmail.com>
2025-11-20 15:53:55 +0530

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,11 @@
+tree1=98
+tree2=96
+tree3=41
+tree4=11
+tree5=94
+
+sum=tree1+tree2+tree3+tree4+tree5
+print("The sum of heights of all trees is:",sum)
+
+average=sum/5
+print("The average of heights of trees is:",average)
